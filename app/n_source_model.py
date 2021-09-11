163c2793f423e6c8d4e5b86e4c10880c77a5c47f
kevaf <kevinkipyegon20@gmail.com>
2021-09-11 10:33:51 +0300

    added Source class

---
+++ app/n_source_model.py
@@ -0,0 +1,11 @@
+class Source:
+    """
+    class that instantiates objects of the source class
+    """
+    def __init__(self, name, description, url, category, language, country):
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category 
+        self.language = language
+        self. country = country
