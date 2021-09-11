fc6182d96a3f770d7b1411ada6c5a039e671343e
kevaf <kevinkipyegon20@gmail.com>
2021-09-11 12:00:23 +0300

    added function to get response from json to url

--- app/request.py
+++ app/request.py
@@ -14,3 +14,23 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     source_url = app.config['NEWS_API_SOURCE_URL']
     articles_url = app.config['NEWS_API_ARTICLE_URL']
+
+def get_sources():
+    """
+    function that gets the response from json to our url request 
+    """
+    get_sources_url = source_url.format(api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_result_list = get_sources_response['sources']
+            source_results = process_results(source_result_list)
+    
+    return source_results
+
+
