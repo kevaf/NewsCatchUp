9db7f9da97ba74f9df046d38c11cb00d61a3d69d
kevaf <kevinkipyegon20@gmail.com>
2021-09-13 12:00:02 +0300

    modified manager.py

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY= 'ebfc7f378b3644929cb1c49457b7d575'
+    export NEWS_API_KEY= 'ebfc7f378b3644929cb1c49457b7d575'
 export SECRET_KEY= 'kevaf'
 
-python3.8 manage.py server
+python3.8 manager.py server
